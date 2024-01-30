6b1ce2b9ef24fa2774108d036ac0978ce82b5155
Iago Passaglia <iago210669@gmail.com>
2024-01-29 21:18:29 -0300

    Adicionando função que toca ou pausa o capitulo

--- script.js
+++ script.js
@@ -2,7 +2,25 @@ const botaoPlayPause = document.getElementById('play-pause');
 const audioCapitulo = document.getElementById('audio-capitulo');
 
 const numeroCapitulos = 10;
+let taTocando = 0;
 
-function tocarFaixa(){
-    
+function tocarFaixa() {
+    audioCapitulo.play();
+    this.taTocando = 1;
 }
+
+function pausarFaixa() {
+
+        audioCapitulo.pause();
+        this.taTocando = 0;
+
+}
+
+function tocarOuPausar(){
+    if(this.taTocando == 0){
+        tocarFaixa();
+    }else{
+        pausarFaixa();
+    }
+}
+
